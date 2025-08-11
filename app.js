9e013496136437b371debc9c1b8d2fe1f3066fdc
Luis8455 <lsmelendez84@gmail.com>
2025-08-11 16:30:04 -0600

    elimine una linea

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//Array para almacenar los nombres
+//Array para almacenar los nombres de los amigos
 let amigos = [];
 
 //Funcion para almacenar los nombres
